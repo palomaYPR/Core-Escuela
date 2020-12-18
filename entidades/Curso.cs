20923bc39a96f3d535a5449981249f285cecb97a
Paloma <yppr09@gmail.com>
2020-12-18 11:29:21 -0600

    Props added

--- entidades/Curso.cs
+++ entidades/Curso.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace CoreEscuela.Entidades
 {
     public class Curso
     {
-        public string UniqueId { get; private set; }
+        public Curso(string uniqueId, string nombre, TiposJornada jornada) 
+        {
+            this.UniqueId = uniqueId;
+                this.Nombre = nombre;
+                this.Jornada = jornada;
+               
+        }
+                public string UniqueId { get; private set; }
         public string Nombre { get; set; }
         public TiposJornada Jornada { get; set; }
-        
+        public List<Asignatura> Asignaturas { get; set; }
+        public List<Alumno> Alumnos { get; set; }
         public Curso()=> UniqueId = Guid.NewGuid().ToString();
         
     }
--- entidades/Evaluaciones.cs
+++ entidades/Evaluaciones.cs
@@ -7,6 +7,9 @@ namespace CoreEscuela.Entidades
         public string UniqueId { get; private set; }
         
         public string Nombre { get; set; }
+        public Alumno Alumno { get; set; }     
+        public Asignatura Asignatura { get; set; }   
+        public float Nota { get; set; }
 
         public Evaluaciones()=> UniqueId = Guid.NewGuid().ToString();   
     }
